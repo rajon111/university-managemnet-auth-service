1710eba74e4dbf8a2d628716a0cb17d263adaeee
rajon111 <mostofarajon111@gmail.com>
2023-06-04 11:11:22 +0600

    n[DONE] setup error Handling

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application } from 'express'
 import cors from 'cors'
 import usersRouter from './app/modules/users/users.router'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
 const app: Application = express()
 
 app.use(cors())
@@ -13,8 +14,12 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/api/v1/users/', usersRouter)
 
 // testing
-app.get('/', async (req: Request, res: Response) => {
-  res.send('Working Successfully!')
-})
+// app.get('/', async (req: Request, res: Response, next:NextFunction) => {
+//   throw new ApiError(400,'some error')
+//   // res.send('Working Successfully!')
+// })
+
+// global errors handler
+app.use(globalErrorHandler)
 
 export default app
---
+++ src/errors/handleValidationaaaaaearror.ts
@@ -0,0 +1,25 @@
+import { IGenericErrorResponse } from '../interfaces/common'
+import { IGenericErrorMessage } from './../interfaces/error'
+import mongoose from 'mongoose'
+
+const handleValidationError = (
+  err: mongoose.Error.ValidationError
+): IGenericErrorResponse => {
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+  const errors: IGenericErrorMessage[] = Object.values(err.errors).map(
+    (el: mongoose.Error.ValidatorError | mongoose.Error.CastError) => {
+      return {
+        path: el?.path,
+        message: el?.message,
+      }
+    }
+  )
+  const statusCode = 400
+  return {
+    statusCode,
+    message: 'Validation Error',
+    errorMessages: errors,
+  }
+}
+
+export default handleValidationError
